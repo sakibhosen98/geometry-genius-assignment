3c98d7a32e7e3aef2151bd1a79eb852c4e9b8a74
sakibhosen98 <sakibhosen9815@gmail.com>
2023-02-18 12:54:38 +0600

    added all card calculation

--- js/calculate.js
+++ js/calculate.js
@@ -6,7 +6,7 @@ document.getElementById('btn-triagle').addEventListener('click', function(){
   const firstName = getTextValueById('first-name');
   const triagleLand = getInputValueById('triangle-land');
   const triagleHeight = getInputValueById('triangle-height');
-  const triagleArea = 0.5 * triagleLand * triagleHeight;
+  const triagleArea = (0.5 * triagleLand * triagleHeight);
   
   // Create element and appendChild
   const container = document.getElementById('table-container');
@@ -28,7 +28,7 @@ document.getElementById('btn-rectangle').addEventListener('click', function(){
   const secondName = getTextValueById('second-name');
   const rectangleWidth = getInputValueById('rectangle-width');
   const rectangleLength = getInputValueById('rectangle-length');
-  const rectangleArea = rectangleWidth * rectangleLength;
+  const rectangleArea = (rectangleWidth * rectangleLength).toFixed(2);
   
   //Create element and Appendchild-----------
   const container = document.getElementById('table-container');
@@ -43,18 +43,120 @@ document.getElementById('btn-rectangle').addEventListener('click', function(){
   container.appendChild(tr);
 })
 
+// Parallelogram
+
+document.getElementById('btn-parallelogram').addEventListener('click', function(){
+  serial += 1;
+  const thirdName = getTextValueById('third-name');
+  const parallelogramLand = getInputValueById('parallelogram-land');
+  const parallelogramHeight = getInputValueById('parallelogram-height');
+  const parallelogramArea = (parallelogramLand * parallelogramHeight).toFixed(2);
+  
+  const container = document.getElementById('table-container');
+  const tr = document.createElement('tr');
+  tr.innerHTML = `
+    <td> ${serial} </td>
+    <td> ${thirdName} </td> 
+    <td> ${parallelogramArea}<span>cm</span><sup>2</sup></td>
+    <td> <button class="bg-blue-500 px-2 py-1 text-white rounded-xl">Convert to m<span>2</span></button>
+    </td>
+  `
+  container.appendChild(tr);
+})
+
+// Rhombus 
+
+  document.getElementById('btn-rhombus').addEventListener('click', function(){
+    serial += 1;
+    const forthName = getTextValueById('forth-name');
+    const rhombusD1 = getInputValueById('rhombus-d1');
+    const rhombusD2 = getInputValueById('rhombus-d2');  
+    const rhombusArea = (0.5 * rhombusD1 * rhombusD2).toFixed(2);
+    
+    const container = document.getElementById('table-container');
+    const tr = document.createElement('tr');
+    tr.innerHTML = `
+      <td> ${serial} </td>
+      <td> ${forthName} </td> 
+      <td> ${rhombusArea}<span>cm</span><sup>2</sup></td>
+      <td> <button class="bg-blue-500 px-2 py-1 text-white rounded-xl">Convert to m<span>2</span></button>
+      </td>
+    `
+    container.appendChild(tr);
+  })
+
+  // Pentagon
+  
+  document.getElementById('btn-Pentagon').addEventListener('click', function(){
+    serial += 1;
+    const fifthName = getTextValueById('fifth-name');
+    const PentagonP = getInputValueById('pentagon-p');
+    const pentagonB = getInputValueById('pentagon-b');
+    const pentagonArea = (0.5 * PentagonP * pentagonB).toFixed(2);
+
+    const container = document.getElementById('table-container');
+    const tr = document.createElement('tr');
+    tr.innerHTML = `
+      <td> ${serial} </td>
+      <td> ${fifthName} </td> 
+      <td> ${pentagonArea}<span>cm</span><sup>2</sup></td>
+      <td> <button class="bg-blue-500 px-2 py-1 text-white rounded-xl">Convert to m<span>2</span></button>
+      </td>
+    `
+    container.appendChild(tr);
+  })
+
+  // Ellipse
+
+  document.getElementById('btn-ellipse').addEventListener('click', function(){
+    serial += 1;
+    const sixthName = getTextValueById('sixth-name');
+    const ellipseA = getInputValueById('ellipse-a');
+    const ellipseB = getInputValueById('ellipse-b');
+    const ellipseArea = (3.14 * ellipseA * ellipseB).toFixed(2);
+    
+    const container = document.getElementById('table-container');
+    const tr = document.createElement('tr');
+    tr.innerHTML = `
+      <td> ${serial} </td>
+      <td> ${sixthName} </td> 
+      <td> ${ellipseArea}<span>cm</span><sup>2</sup></td>
+      <td> <button class="bg-blue-500 px-2 py-1 text-white rounded-xl">Convert to m<span>2</span></button>
+      </td>
+    `
+    container.appendChild(tr);
+
+  })
 
 
 // =============================================
-// function -1
+// function -1: for get input value
 function getInputValueById(idName){
   const inputElement = document.getElementById(idName).value;
-  const value = parseInt(inputElement);
+  const value = parseFloat(inputElement);
   return value;
   }
 
-  // function-2
+  // function-2: for get innerText value
   function getTextValueById(idName){
     const textElement = document.getElementById(idName).innerText;
     return textElement;
   }
+
+
+  // // function -3: for bg random color
+  // function randomColor() {
+  //   let color = [];
+  //   for (let i = 0; i < 3; i++) {
+  //     color.push(Math.floor(Math.random() * 256));
+  //   }
+  //   return 'rgb(' + color.join(', ') + ')';
+  // } 
+  // document.addEventListener("mouseover", function(){
+
+  //   document.body.style.backgroundColor = randomColor();
+  // });
+  
+  // document.querySelector('circle').addEventListener('mouseover', event => {
+  //       event.target.style.fill = randomColor();
+  // });
