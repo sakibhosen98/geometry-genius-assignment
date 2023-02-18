84abbd7a30d934e6b070c33c96e6bab0c2c55430
sakibhosen98 <sakibhosen9815@gmail.com>
2023-02-18 18:16:15 +0600

    added validation check

--- js/calculate.js
+++ js/calculate.js
@@ -1,3 +1,6 @@
+
+// Card callcultion start:
+// Triangle
 let serial = 0;
 document.getElementById('btn-triagle').addEventListener('click', function(){
   serial += 1;
@@ -23,30 +26,6 @@ document.getElementById('btn-triagle').addEventListener('click', function(){
   container.appendChild(tr);
 
 })
-
-// ---------------------
-
-// Triangle
-// let serial = 0;
-// document.getElementById('btn-triagle').addEventListener('click', function(){
-//   serial += 1;
-//   const firstName = getTextValueById('first-name');
-//   const triagleLand = getInputValueById('triangle-land');
-//   const triagleHeight = getInputValueById('triangle-height');
-//   const triagleArea = (0.5 * triagleLand * triagleHeight);
-  
-//   // Create element and appendChild
-//   const container = document.getElementById('table-container');
-//   const tr = document.createElement('tr');
-//   tr.innerHTML = `
-//     <td> ${serial} </td>
-//     <td> ${firstName} </td> 
-//     <td> ${triagleArea}<span>cm</span><sup>2</sup></td>
-//     <td> <button class="bg-blue-500 px-2 py-1 text-white rounded-xl">Convert to m<span>2</span></button>
-//     </td>
-//   `
-//   container.appendChild(tr);
-// })
 
 // Rectangle
 
